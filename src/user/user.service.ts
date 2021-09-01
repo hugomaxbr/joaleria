17d19ee72b1d3eae14c46bf22c22dc8aefaea583
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2021-09-01 01:04:57 -0300

    style: switch to single quotes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,27 +2,27 @@ import {
   Injectable,
   ConflictException,
   NotFoundException,
-} from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { UserRepository } from "./user.repository";
-import { CreateUserDto } from "./dto/createUserDto";
-import { User } from "./entities/user.entity";
-import { ListUserByIdDto } from "./dto/listUserByIdDto";
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { CreateUserDto } from './dto/createUserDto';
+import { User } from './entities/user.entity';
+import { ListUserByIdDto } from './dto/listUserByIdDto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<void> {
     const userFound = await this.userRepository.findByEmail(
-      createUserDto.email
+      createUserDto.email,
     );
 
     if (userFound) {
-      throw new ConflictException("Email already registered");
+      throw new ConflictException('Email already registered');
     }
 
     this.userRepository.createUserAdmin(createUserDto);
@@ -40,7 +40,7 @@ export class UserService {
     const user = await this.userRepository.findById(deleteUserById.id);
 
     if (!user) {
-      throw new NotFoundException("User not found");
+      throw new NotFoundException('User not found');
     }
 
     await this.userRepository.deleteById(user);
