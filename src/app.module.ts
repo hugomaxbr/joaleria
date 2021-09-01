2b9c3caaba452cc0e05d8c7d68423910754c26d6
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2021-09-01 01:01:04 -0300

    style: switched to single quote

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import typeOrmConfig from "./config/typeorm.config";
-import { ProfileModule } from "./profile/profile.module";
-import { UserModule } from "./user/user.module";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from './config/typeorm.config';
+import { ProfileModule } from './profile/profile.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, ProfileModule],
