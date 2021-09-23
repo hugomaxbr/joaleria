d1ccb52c532138310a8d98ffd2fc11e491813ae0
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2021-09-22 22:56:56 -0300

    feat: add new local service to save files

---
+++ src/upload/local.service.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import { Injectable } from '@nestjs/common';
+import { randomBytes } from 'crypto';
+import { resolve } from 'path';
+
+@Injectable()
+export class LocalService {
+  async uploadFile(file: Express.Multer.File, id: string) {
+    const [, fileExtension] = file.originalname.split('.');
+
+    const fileName = `${id}-${Date.now()}-${randomBytes(12).toString(
+      'hex',
+    )}.${fileExtension}`;
+
+    const filePath = resolve(__dirname, '..', '..', 'temp', 'images', fileName);
+
+    await fs.promises.writeFile(filePath, file.buffer, {
+      encoding: 'utf-8',
+    });
+
+    return `http://localhost:3333/${fileName}`;
+  }
+}
