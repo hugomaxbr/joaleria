343c62bf447b4e38dda149dfbfe56e3774bf3c9a
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2021-09-22 22:52:41 -0300

    chore: add new column migration

---
+++ src/config/migrations/1632353113906-AddColumnAvatarUrl.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AddColumnAvatarUrl1632353113906 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar_url',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar_url');
+  }
+}
