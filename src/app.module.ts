14443ac42bc0dc7f5d9e93b2b40a5781bae21a2d
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2021-09-22 22:00:05 -0300

    add Upload Module and static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import typeOrmConfig from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { UploadModule } from './upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, ProductModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UserModule,
+    ProductModule,
+    UploadModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'temp', 'images'),
+    }),
+  ],
 })
 export class AppModule {}
